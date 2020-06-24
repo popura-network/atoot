d812ed781a318651e83666b44414d181d5f1fc89
George <zhoreeq@users.noreply.github.com>
2020-06-24 11:58:43 -0400

    Add long description to setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,13 @@ test_deps = [
     'pytest'
 ]
 
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
 setup(name='atoot',
       version=__version__,
       description='Asynchronous Python library for the Mastodon API',
+      long_description=long_description,
       packages=['atoot'],
       install_requires=[
           'aiohttp>=3.6.2', 
