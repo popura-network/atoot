9c13f36ce58a33fed678257145914107350a9560
George <zhoreeq@users.noreply.github.com>
2020-06-24 14:43:03 -0400

    Correct documentation setup

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,11 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sys, os
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
+
+import atoot
 
 # -- Project information -----------------------------------------------------
 
